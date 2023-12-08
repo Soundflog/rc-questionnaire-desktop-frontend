cec58563b69a0204e509f2bfd2ed70209918bcb3
Soundflog <paha-vershinin@mail.ru>
2023-12-08 18:47:40 +0300

    тестовые данные упражнения

---
+++ src/app/data/exercises.ts
@@ -0,0 +1,32 @@
+import {IExercise} from "../models/exercise";
+
+export const exercises: IExercise[] = [
+    {
+        id: 1,
+        name: "Упражнение 1",
+        description: "Описание упражнения 1",
+        video_url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&t',
+        exercise_type_id: 1
+    },
+    {
+        id: 2,
+        name: "Упражнение 2",
+        description: "Описание упражнения 2",
+        video_url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&t',
+        exercise_type_id: 2
+    },
+    {
+        id: 3,
+        name: "Упражнение 3",
+        description: "Описание упражнения 3",
+        video_url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&t',
+        exercise_type_id: 3
+    },
+    {
+        id: 4,
+        name: "Упражнение 4",
+        description: "Описание упражнения 4",
+        video_url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&t',
+        exercise_type_id: 4
+    },
+    ]
