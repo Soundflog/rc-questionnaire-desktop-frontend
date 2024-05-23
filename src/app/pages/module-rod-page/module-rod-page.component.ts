d4e70d583e695b032c2dfb303097087ca0813aa9
Soundflog <paha-vershinin@mail.ru>
2024-05-23 21:28:38 +0300

    add flex center on module-rod-page

--- src/app/pages/module-rod-page/module-rod-page.component.ts
+++ src/app/pages/module-rod-page/module-rod-page.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-module-rod-page',
   templateUrl: './module-rod-page.component.html',
-  styleUrls: ['./module-rod-page.component.css']
+  styleUrls: ['./module-rod-page.component.css', 'module-rod-page.component.less']
 })
 export class ModuleRodPageComponent {
 
